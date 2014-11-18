f9de1add0ec1a765ce592b6e9099bf3f5b561151
zxbloved <4047499@qq.com>
2014-11-18 15:25:17 +0800

    Initial Commit

---
+++ GoShopping/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoShopping
+//
+//  Created by stevenzxb on 14/11/18.
+//  Copyright (c) 2014年 MAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
